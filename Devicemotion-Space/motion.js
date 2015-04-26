679b7ae2e478e0178217f452a4464b063b3355d6
Richard Armuelles <alexanderarmuelles@hotmail.com>
2015-04-26 11:03:48 -0500

    first push

---
+++ Devicemotion-Space/motion.js
@@ -0,0 +1,72 @@
+function getUuid() {
+     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+         return v.toString(16);
+     });
+   }
+   var deviceId = getUuid();
+   if ('requestWakeLock' in navigator) {
+     var lock = navigator.requestWakeLock('screen');
+     window.addEventListener('visibilitychange', function() {
+       if (document.hidden) {
+         lock.unlock();
+       }
+       else {
+         lock = navigator.requestWakeLock('screen');
+       }
+     });
+   }
+   var counter = 0; // gets upped every time we activate the sensor
+   var wsOpen = false;
+   var ws;
+   function connect() {
+     ws = new WebSocket('ws://10.72.5.126:8322');
+     ws.onopen = () => {
+       wsOpen = true;
+     };
+     ws.onclose = () => {
+       wsOpen = false;
+       connect();
+     };
+   }
+   connect();
+   var odm = function(e) {
+     // Keon logic here, for some reason stable Z is 18
+     var z = Math.abs(e.accelerationIncludingGravity.z);
+     if (z > 10 && z < 25) {
+       // stable
+       document.body.style.backgroundColor = 'gray';
+     }
+     else if (z < 10) {
+       // free fall
+       document.body.style.backgroundColor = 'red';
+     }
+     else {
+       // goes up
+       document.body.style.backgroundColor = 'green';
+     }
+     document.querySelector('h1').textContent = e.accelerationIncludingGravity.z | 0;
+     console.log(e.accelerationIncludingGravity.z, +new Date());
+     // return;
+     if (wsOpen) {
+       ws.send(JSON.stringify({
+         type: 'devicemotion',
+         deviceId: deviceId + counter,
+         x: e.accelerationIncludingGravity.x,
+         y: e.accelerationIncludingGravity.y,
+         z: e.accelerationIncludingGravity.z
+       }));
+     }
+   };
+
+    var active = false;
+    document.querySelector('button').oncontextmenu = function() {
+      counter++;
+      if (active) {
+        window.removeEventListener('devicemotion', odm);
+      }
+      else {
+        window.addEventListener('devicemotion', odm);
+      }
+      active = !active;
+    };
---
+++ servers/server-fxos-doorbell.js
@@ -0,0 +1,24 @@
+var connect = require('connect');
+connect()
+.use(connect.static(__dirname))
+.listen(8321, '0.0.0.0');
+
+var ws = require("nodejs-websocket");
+var server = ws.createServer(function(conn) {
+	console.log("New connection");
+	conn.on("text", function(str) {
+		console.log("Received " + str);
+		broadcast(str);
+	});
+	conn.on("close", function(code, reason) {
+		console.log("Connection closed");
+	});
+}).listen(8322, '0.0.0.0');
+
+function broadcast(msg) {
+	server.connections.forEach(function(conn) {
+		conn.sendText(msg);
+	});
+}
+
+console.log('Listening on port', 8321, 8322);
